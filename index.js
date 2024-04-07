d357fbb1cbb0e8b7f464d3967eb4e432366aa156
Mehedi Hasan <mh.ju1803@gmail.com>
2024-04-07 14:59:15 +0600

    Timer added

--- index.js
+++ index.js
@@ -61,3 +61,28 @@ function appendItemToShoppingListEl(item) {
     })
     shoppingListEl.append(newEl)
 }
+
+const newYears = '8 Apr 2024';
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+        hoursEl.innerHTML = formatTime(hours)
+        minutesEl.innerHTML = formatTime(minutes)
+        secondsEl.innerHTML = formatTime(seconds)
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+countdown();
+setInterval(countdown, 1000)
